fbff5263ee88a2caaef614f9a83bc563f6f58411
kushagra <kushagratripathi7@gmail.com>
2023-01-10 04:05:48 +0530

    fix:polar chart

--- src/ChartComponents/PolarArea.tsx
+++ src/ChartComponents/PolarArea.tsx
@@ -2,30 +2,42 @@ import { useState } from "react";
 import ReactApexChart from "react-apexcharts";
 
 const PolarArea = () => {
-  const [series, setSeries] = useState([14, 23, 21, 17, 15, 10, 12]);
+  const [series, setSeries] = useState([100, 60, 100, 50, 80, 50, 10, 40]);
 
   const [options, setOptions] = useState({
-    stroke: {
-      colors: ["#F6F6F6"],
-    },
     fill: {
       opacity: 0.8,
+      colors: [
+        ({ value }: any) => {
+          if (value > 90) {
+            return "#9055FF";
+          }
+          if (value > 50 && value < 90) {
+            return "#FFCAC9";
+          } else {
+            return "rgba(225, 225, 225, 0.6)";
+          }
+        },
+      ],
     },
     responsive: [
       {
-        breakpoint: 480,
+        breakpoint: undefined,
         options: {
           chart: {
             width: "100%",
           },
-          legend: {
-            show: false,
-            // position: "bottom",
-          },
+          // legend: {
+          //   // show: false,
+          //   // position: "bottom",
+          // },
         },
       },
     ],
+    startAngle: 270,
   });
+
+  console.log(typeof options.fill.colors);
   return (
     <div id="chart">
       <ReactApexChart options={options} series={series} type="polarArea" />
